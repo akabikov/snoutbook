31707e94e4add90dcc90d8ac4cede97b5b90feda
Alexey Kabikov <kabikov.a.s@gmail.com>
2020-06-14 19:35:25 +0300

    style: styling navbar

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,27 +1,46 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import data from "./data.json";
 
 class NavBar extends React.Component {
   render() {
     const { listName } = this.props;
     const links = data[listName].map(({ name, src }) => (
-      <NavLink
-        exact
-        to={`/${listName}/${src}`}
-        activeClassName='nav-active'
-        key={src}
-      >
-        {name}
-      </NavLink>
+      <li className='nav-item' key={src}>
+        <NavLink exact to={`/${listName}/${src}`} className='nav-link'>
+          {name}
+        </NavLink>
+      </li>
     ));
 
     return (
-      <nav>
-        <NavLink exact to={listName} activeClassName='nav-active'>
-          Home
-        </NavLink>
-        {links}
+      <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
+        <Link className='navbar-brand' to={listName}>
+          {listName.replace(/^\w/, (m) => m.toUpperCase())}
+        </Link>
+
+        <button
+          className='navbar-toggler'
+          type='button'
+          data-toggle='collapse'
+          data-target='#navbarNav'
+          aria-controls='navbarNav'
+          aria-expanded='false'
+          aria-label='Toggle navigation'
+        >
+          <span className='navbar-toggler-icon' />
+        </button>
+
+        <div className='collapse navbar-collapse' id='navbarNav'>
+          <ul className='navbar-nav'>
+            <li className='nav-item'>
+              <NavLink exact to={listName} className='nav-link'>
+                Home
+              </NavLink>
+            </li>
+            {links}
+          </ul>
+        </div>
       </nav>
     );
   }
