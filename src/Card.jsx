df6d1ed777fe5a2f5528d7d82a78e2c22a17562d
Alexey Kabikov <kabikov.a.s@gmail.com>
2020-06-14 22:43:20 +0300

    style: styling card

--- src/Card.jsx
+++ src/Card.jsx
@@ -10,16 +10,30 @@ class Card extends React.Component {
 
     if (!item) return <Redirect to={`/${listName}`} />;
 
-    const { name, age, facts } = item;
-    const factsList = facts.map((fact, i) => <p key={i}>{fact}</p>);
+    const { name, descr, facts } = item;
+    const factsList = facts.map((fact, i) => (
+      <li className='list-group-item' key={i}>
+        {fact}
+      </li>
+    ));
 
     return (
-      <div>
-        <img src={`/img/${src}.jpg`} alt={name} />
-        <h2>{name}</h2>
-        <p>{age} years old</p>
-        {factsList}
-        <button onClick={history.goBack}>Go Back</button>
+      <div className='row justify-content-center mt-5'>
+        <div className='col-11 col-lg-5'>
+          <div className='card'>
+            <img className='card-img-top' src={`/img/${src}.jpg`} alt={name} />
+            <div className='card-body'>
+              <h2 className='card-title'>{name}</h2>
+              <h4 className='card-subtitle text-muted'>{descr}</h4>
+            </div>
+            <ul className='list-group list-group-flush'>{factsList}</ul>
+            <div className='card-body'>
+              <button className='btn btn-info' onClick={history.goBack}>
+                Go Back
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
