3665a70fbcf8236eb4de6ae5f6ef93399b3d8551
Alexey Kabikov <kabikov.a.s@gmail.com>
2020-06-15 19:11:59 +0300

    fix: navbar collapsing and home links path

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -6,7 +6,12 @@ class NavBar extends React.Component {
   render() {
     const { listName } = this.props;
     const links = data[listName].map(({ name, src }) => (
-      <li className='nav-item' key={src}>
+      <li
+        className='nav-item'
+        data-toggle='collapse'
+        data-target='#navbarNav'
+        key={src}
+      >
         <NavLink exact to={`/${listName}/${src}`} className='nav-link'>
           {name}
         </NavLink>
@@ -15,7 +20,7 @@ class NavBar extends React.Component {
 
     return (
       <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
-        <Link className='navbar-brand' to={listName}>
+        <Link className='navbar-brand' to={`/${listName}`}>
           {listName.replace(/^\w/, (m) => m.toUpperCase())}
         </Link>
 
@@ -33,8 +38,12 @@ class NavBar extends React.Component {
 
         <div className='collapse navbar-collapse' id='navbarNav'>
           <ul className='navbar-nav'>
-            <li className='nav-item'>
-              <NavLink exact to={listName} className='nav-link'>
+            <li
+              className='nav-item'
+              data-toggle='collapse'
+              data-target='#navbarNav'
+            >
+              <NavLink exact to={`/${listName}`} className='nav-link'>
                 Home
               </NavLink>
             </li>
