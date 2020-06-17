d12237be609f040df417ebb7c29ef2fe54b0ca9b
Alexey Kabikov <kabikov.a.s@gmail.com>
2020-06-17 19:21:50 +0300

    Updates

---
+++ precache-manifest.f7e68a8d8c8d27cf4dc0a0916f780094.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cdb1c38bc477a6ea18006b6ee1b11ea",
+    "url": "/snoutbook/index.html"
+  },
+  {
+    "revision": "ec99c515dd6766542f2d",
+    "url": "/snoutbook/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "69d1bcb5d3edfac98293",
+    "url": "/snoutbook/static/css/main.856352b4.chunk.css"
+  },
+  {
+    "revision": "ec99c515dd6766542f2d",
+    "url": "/snoutbook/static/js/2.64163497.chunk.js"
+  },
+  {
+    "revision": "618d9e067b5f7c0ee048a43ae1109b7d",
+    "url": "/snoutbook/static/js/2.64163497.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69d1bcb5d3edfac98293",
+    "url": "/snoutbook/static/js/main.23e251d8.chunk.js"
+  },
+  {
+    "revision": "018db60344dafa4eb6c6",
+    "url": "/snoutbook/static/js/runtime-main.abe04ca5.js"
+  }
+]);
